16c13bd38791d3419eaa8894ad2f7bacd99bc94d
OpenBot <service@BOTCoin.Network>
2025-04-17 16:59:25 +0800

    1.0.10

--- src/commands/poa-stake.ts
+++ src/commands/poa-stake.ts
@@ -1,13 +1,19 @@
 import Node, { Contract } from 'evm-lite-core';
-import { Currency } from 'evm-lite-utils';
+import Datadir from 'evm-lite-datadir';
+import { Currency, IUnits } from 'evm-lite-utils';
 import Inquirer from 'inquirer';
 import Vorpal from 'vorpal';
+import color from '../core/color';
 import Session from '../core/Session';
 import Command, { Arguments, TxOptions } from '../core/TxCommand';
 
 type Opts = TxOptions & {
     host: string;
     port: number;
+
+    pwd?: string;
+    from: string;
+    value: string;
 };
 
 type Args = Arguments<Opts> & {
@@ -18,6 +24,10 @@ type Answers = {
     amount: string;
 };
 
+function isLetter(str: string) {
+	return str.length === 1 && str.match(/[a-z]/i);
+}
+
 export default (evmlc: Vorpal, session: Session) => {
     const description = 'Stake BOC tokens to participate in consensus';
 
@@ -27,6 +37,8 @@ export default (evmlc: Vorpal, session: Session) => {
         .description(description)
         .option('-i, --interactive', 'enter interactive')
         .option('-d, --debug', 'show debug output')
+        .option('--from <moniker>', 'from moniker')
+        .option('--pwd <password>', 'passphrase file path')
         .option('-g, --gas <g>', 'override config gas value')
         .option('-h, --host <ip>', 'override config host value')
         .option('-p, --port <port>', 'override config port value')
@@ -54,6 +66,8 @@ class StakeCommand extends Command<Args> {
         if (!this.args.options.gas && this.args.options.gas !== 0) {
             this.args.options.gas = this.config.defaults.gas;
         }
+        this.args.options.from =
+			this.args.options.from || this.config.defaults.from;
 
         this.node = new Node(this.args.options.host, this.args.options.port);
 
@@ -61,8 +75,13 @@ class StakeCommand extends Command<Args> {
     }
 
     protected async prompt(): Promise<void> {
+        const keystore = await this.datadir.listKeyfiles();
         const questions: Inquirer.QuestionCollection<Answers> = [
             {
+                default:
+					(this.args.options.from &&
+						keystore[this.args.options.from].address) ||
+					'',
                 message: 'Stake amount (BOC): ',
                 name: 'amount',
                 type: 'input',
@@ -74,14 +93,35 @@ class StakeCommand extends Command<Args> {
         ];
 
         const answers = await Inquirer.prompt<Answers>(questions);
-        this.args.amount = answers.amount;
+
+        this.args.options.value = answers.amount;
+
+		const u = this.args.options.value.toString().slice(-1) as IUnits;
+		if (!isLetter(u)) {
+			this.args.options.value = this.args.options.value + 'T';
+		}
     }
 
     protected async check(): Promise<void> {
-        const amount = parseFloat(this.args.amount);
-        if (isNaN(amount) || amount <= 100000) {
+        
+        if (!this.args.options.value) {
+			throw Error('Provide `to` address and `value` to send');
+		}
+
+        if (parseFloat(this.args.options.value) <= 100000) {
             throw Error('Invalid stake amount. Must stake more than 100,000 BOC');
         }
+        if (!this.account) {
+			if (!this.args.options.from) {
+				throw Error('No `from` moniker provided or set in config.');
+			}
+
+			if (!this.passphrase) {
+				if (!this.args.options.pwd) {
+					throw Error('Passphrase file path not provided.');
+				}
+			}
+		}
     }
 
     protected async exec(): Promise<string> {
@@ -96,16 +136,33 @@ class StakeCommand extends Command<Args> {
 
 		const contract = Contract.load(JSON.parse(poa.abi), poa.address);
 
+        // sanity check
+        if (!this.account) {
+            const keyfile = await this.datadir.getKeyfile(
+                this.args.options.from
+            );
+
+            this.account = Datadir.decrypt(keyfile, this.passphrase!);
+        }
+
         this.debug('Generating stake transaction');
+ 
         const tx = contract.methods.stake(
             {
+                from: this.account.address,
                 gas: this.args.options.gas,
                 gasPrice: Number(this.args.options.gasPrice),
-                value: new Currency(this.args.amount).format('a').slice(0, -1)
+                value: new Currency(this.args.options.value)
             }
         );
 
+        color.yellow(JSON.stringify(tx, null, 2));
+        color.yellow(
+            `Transaction fee: ${Number(this.args.options.gasPrice)}`
+        );     
+
         this.debug('Sending transaction');
+
         const response = await this.node!.sendTx(tx, poa.address);
 
         if (this.args.options.json) {
